b77e792e6236a1983b7c51cafb081adcaa56ba26
LinaPeralta <69905738+LinaPeralta@users.noreply.github.com>
2021-10-01 20:12:29 -0500

    clase datos

--- app/src/main/java/com/example/generador/MainActivity.java
+++ app/src/main/java/com/example/generador/MainActivity.java
@@ -6,6 +6,15 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -18,6 +27,14 @@ public class MainActivity extends AppCompatActivity {
     private EditText cantidadTxt;
     private EditText posxTxt;
     private EditText posyTxt;
+
+    private String json;
+
+    BufferedReader bfr;
+    BufferedWriter bfw;
+
+    Dato datos;
+   // Gson gson;
 
 
     @Override
@@ -36,6 +53,73 @@ public class MainActivity extends AppCompatActivity {
         posxTxt = findViewById(R.id.posxTxt);
         posyTxt = findViewById(R.id.posyTxt);
 
+        datos =  new Dato();
+       // gson = new Gson();
+
+
+        new Thread(
+                ()->{
+                    try {
+                        Socket socket = new Socket("10.0.2.2",9000);
+
+                        InputStream is = socket.getInputStream();
+                        InputStreamReader isr =  new InputStreamReader(is);
+                        bfr = new BufferedReader(isr);
+
+                        OutputStream os = socket.getOutputStream();
+                        OutputStreamWriter osw =  new OutputStreamWriter(os);
+                        bfw= new BufferedWriter(osw);
+
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+        ).start();
+
+
+        rojoBtn.setOnClickListener(
+                (v) -> {
+                   // datos.setNombre();
+                   // datos.setMov(20);
+                   // json = gson.toJson(datos);
+                    //enviarMensaje(json);
+                }
+        );
+
+        verdeBtn.setOnClickListener(
+                (v) -> {
+                    // datos.setMovimiento("up");
+                    // datos.setMov(20);
+                    // json = gson.toJson(datos);
+                    //enviarMensaje(json);
+                }
+        );
+
+        azulBtn.setOnClickListener(
+                (v) -> {
+                    // datos.setMovimiento("up");
+                    // datos.setMov(20);
+                    // json = gson.toJson(datos);
+                    //enviarMensaje(json);
+                }
+        );
+
+
+    }
+    public void enviarMensaje (String sms) {
+
+        new Thread(()->{
+            try {
+                bfw.write(sms + "\n");
+                bfw.flush();
+
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }).start();
 
 
     }
