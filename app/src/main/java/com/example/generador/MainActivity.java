12805b8b7f7948979eb099d81dae62ea26166aa0
LinaPeralta <69905738+LinaPeralta@users.noreply.github.com>
2021-10-01 19:38:14 -0500

    inicializar

--- app/src/main/java/com/example/generador/MainActivity.java
+++ app/src/main/java/com/example/generador/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.generador;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button crearBtn;
+    private Button borrarBtn;
+    private Button rojoBtn;
+    private Button verdeBtn;
+    private Button azulBtn;
+    private EditText nombreTxt;
+    private EditText cantidadTxt;
+    private EditText posxTxt;
+    private EditText posyTxt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        crearBtn = findViewById(R.id.crearBtn);
+        borrarBtn = findViewById(R.id.borrarBtn);
+        rojoBtn = findViewById(R.id.rojoBtn);
+        verdeBtn = findViewById(R.id.verdeBtn);
+        azulBtn = findViewById(R.id.azulBtn);
+        nombreTxt = findViewById(R.id.nombreTxt);
+        cantidadTxt = findViewById(R.id.cantidadTxt);
+        posxTxt = findViewById(R.id.posxTxt);
+        posyTxt = findViewById(R.id.posyTxt);
+
+
+
     }
 }
